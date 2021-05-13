f1c76a316347522072e612d037f3005ba4dca5d5
thiagoassisk8 <tyagosilva82@gmail.com>
2021-05-13 10:54:39 -0300

    flight kira spaw e ninjastar

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    // public GameObject[] objectSpawn;
 
     struct SpawnTime {
         public float instantiateTime;
@@ -26,21 +27,32 @@ public class EnemySpawn : MonoBehaviour
     public float speed = 4;
 
     void Start() {
+        
 
         flying.instantiateTime = 0;
         flying.interval = 0.7f;
         flying.variation = 0.5f;
+        // InvokeRepeating("Spawn", flying.interval, flying.variation);
 
     }
+    
+    // void Spawn(){
+        // int randIndex = (int)Random.Range(0,objectSpawn.Length);
+        // Instantiate(objectSpawn[randIndex], transform.position, Quaternion.identity);
+    // }
 
     void Update() {
+        
 
         // spawn flying
         if (Time.time > flying.instantiateTime && !stopSpawn) {
             GameObject obj = Instantiate(flyingPrefabRef, new Vector3(5, Random.Range(flyingMax, flyingMin)), Quaternion.identity);
             flying.instantiateTime = Time.time + Random.Range(flying.interval - flying.variation, flying.interval + flying.variation);
+            // Start();
+            // Spawn();
         }
         
 
     }
+    
 }
--- Assets/Scripts/ObjectDestroyer.cs
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -4,18 +4,34 @@ using System.Security.Cryptography;
 using UnityEngine;
 
 public class ObjectDestroyer : MonoBehaviour {
-
+    public GameObject[] objectSpawn;
     public GameObject platformDescructionPoint;
+    
+    
 
     // Use this for initialization
     void Start () {
-        platformDescructionPoint = GameObject.Find("PlatformDescructionPoint");
+        platformDescructionPoint = GameObject.Find("PlatformDescructionPoint");        
+        InvokeRepeating("Spawn", 5, 5);
+        
+            
     }
+    
+    void Spawn(){
+        
+        for (int i=0;i<=8;i++){
+            Instantiate(objectSpawn[0], transform.position, Quaternion.identity);
+        }
+        
+    }    
 	
     // Update is called once per frame
     void Update () {
+        
         if (transform.position.x < platformDescructionPoint.transform.position.x) {
             gameObject.SetActive(false);
+            
         }
+        
     }
 }
